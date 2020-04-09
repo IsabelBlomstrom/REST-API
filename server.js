01b08e56acc4e1e133683a010fc92e5d2a49c3ee
Isabel Blomström <isabel.blomstrom@gmail.com>
2020-04-09 11:43:12 +0200

    Solved all logic but delete

--- server.js
+++ server.js
@@ -1,16 +1,63 @@
 const express = require("express")
-const server = express()
-const hostname = "localhost"
+const app = express()
 const port = 3000
 
-server.get("/", (req, res) => {
-  res.send("hej hallå!")
+const books = [{
+    id: 1,
+    title: "Den bästa boken",
+    author: "Isabel Blomström",
+    released: 2010
+  },
+  {
+    id: 2,
+    title: "Den sämsta boken",
+    author: "Neil Strauss",
+    released: 2005
+  },
+  {
+    id: 3,
+    title: "Boken som inte är en bok",
+    author: "Anonymous",
+    released: 0
+  }
+]
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.json("startsida")
 })
 
-server.post("/", () => {
-  res.send("POST - hallooooh")
+app.get("/books", (req, res) => {
+  res.json(books)
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server is running perfectly at: http://${hostname}:${port}`)
+app.get("/books/:id", (req, res) => {
+  const book = books.find(book => book.id == req.params.id)
+  if(!book) res.status(404).send("Boken finns inte")
+  res.send(book)
+ })
+
+//Lägger till
+app.post("/books", (req, res) => {
+  books.push(req.body)
+  res.status(201).send()
+})
+
+//Ändrar
+app.put("/books/:id", (req, res) => {
+ const book = books.find(b => b.id == req.params.id)
+ if(!book) res.status(404).send("Boken finns inte")
+
+ book.title = req.body.title
+ book.author = req.body.author
+ res.send(book)
+})
+
+app.delete("/books/:id", (req, res) => {
+  res.json(req.body)
+})
+
+app.listen(port, () => {
+  console.log(`Server is running perfectly at: http://localhost:${port}/books`)
 })
